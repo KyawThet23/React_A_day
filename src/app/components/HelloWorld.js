ac830861798793ef9f294b0a2bc83368a7544e06
kyawThetHtun <kyawthe235@gmail.com>
2024-07-21 23:39:57 +0630

    first commit

---
+++ src/app/components/HelloWorld.js
@@ -0,0 +1,17 @@
+function AnotherChild()
+{
+  return (<div>
+    Another Child
+  </div>)
+}
+
+export default function HelloWorld()
+{
+  return(
+    <div>
+      <h1>Hello World Components</h1>
+      <p>Hi How are you?</p>
+      <AnotherChild />
+    </div>
+  )
+}
